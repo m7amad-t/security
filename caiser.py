ec8278217ff055df858225bf69973e4aa4302c9c
Mohammad <99071291+m7amad-t@users.noreply.github.com>
2024-05-22 01:47:08 +0300

    Update caiser.py

--- caiser.py
+++ caiser.py
@@ -1,3 +1,17 @@
+def caesarDecipher(ciphertext, key):
+    result = ""
+    for char in ciphertext:
+        if char.isalpha():
+            ascii_val = ord(char)
+            if char.isupper():
+                shifted_char = chr((ascii_val - 65 - key) % 26 + 65)
+            else:
+                shifted_char = chr((ascii_val - 97 - key) % 26 + 97)
+            result += shifted_char
+        else:
+            result += char
+    return result
+
 def caesarCipher(plaintext, key):
     result = ""
     for char in plaintext:
@@ -11,9 +25,11 @@ def caesarCipher(plaintext, key):
         else:
             result += char
     return result
+
 text = "Information Security"
 key = 4
 encryptedMessage = caesarCipher(text, key)
 print("Encrypted Message Is : ", encryptedMessage)
-decryptedMessage = caesarCipher(encryptedMessage, -key)
+
+decryptedMessage = caesarDecipher(encryptedMessage, key)
 print("Decrypted Message Is : ", decryptedMessage)
